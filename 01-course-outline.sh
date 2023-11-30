d1b9733a27631dc6a34b48c4383e25b150b4f6d1
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-30 22:21:09 +0600

    course assigned

---
+++ 01-course-outline.sh
@@ -0,0 +1,11 @@
+# 1. Introduction
+# 2. Version Control System (VCS)
+# 3. Git (Download & Install), Configuration
+# 4 Remote VCS (GitHub, GitLab, SVN (SunVersion))
+# 5. GitHub
+# 5.1. Account Setup
+# 5.2. Connecting to Git
+# 5.3. Collaboration
+# 5.4. GitHub Functionalities 
+# such as
+# (watch, history, issues, branches, conflicts resolution, merge, merge conflicts).
