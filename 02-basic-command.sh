8dcd41bc85041f100bb7c932595ce36d4c6e8db6
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-30 23:03:58 +0600

    basic setup done

---
+++ 02-basic-command.sh
@@ -0,0 +1,14 @@
+# git version check
+# git --version
+# check the command and it details
+# git or gir --help
+
+# check the config list
+# git config --list
+
+# setup the username and email
+# git config --global user.name "jakir-ruet"
+# git config --global user.email "jakir.ruet.bd@gmail.com"
+
+# setup the defalut branch
+# git config --global init.defaultBranch master
